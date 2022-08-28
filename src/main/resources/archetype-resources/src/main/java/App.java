aecf898d7a8b28255ef73488999ae3f36e6efc83
Joao Alba <joaop.alba@gmail.com>
2022-08-28 19:28:46 -0300

    Criação do projeto.

---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $br.furb;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
